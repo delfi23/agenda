92129bc57dffa49dbf0c68eac1770ba71cb5adb9
Delfina Troncoso <78504475+delfi23@users.noreply.github.com>
2021-11-05 18:39:55 -0300

    Upload files

---
+++ agendaElectronica/src/agendaElectronica/agendaElectronica.java
@@ -0,0 +1,21 @@
+
+// Este paquete corresponde a la lógica
+package troncoso_delfina_tpo1;
+
+import IGU.Principal;
+
+public class Troncoso_delfina_tpo1 {
+
+    public static void main(String[] args) {
+        
+        //Crear la pantalla
+        Principal princ = new Principal();
+        
+        //Muestra la pantalla
+        princ.setVisible(true);
+        
+        //Centra la pantalla
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
